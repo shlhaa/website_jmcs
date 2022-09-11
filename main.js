2c14c6026415ccf4217e72231ab17237eedb0ec5
shlhaa <113224792+shlhaa@users.noreply.github.com>
2022-09-11 13:40:57 +0800

    index.html

---
+++ main.js
@@ -0,0 +1,15 @@
+           function myFunc1() {
+               document.getElementById("img1").src = "/image/Html-1.jpg";
+               document.getElementById("h2").innerHTML = " Defination of HTML ";
+               document.getElementById("demo").innerHTML = "' " + "HTML" + " '" + "<br>" + "<br>" + "• HTML stands for HyperText Markup Language. It is a standard markup language for web page creation. It allows the creation and structure of sections, paragraphs, and links using HTML elements (the building blocks of a web page) such as tags and attributes." + "<br>";
+           }
+           function myFunc2() {
+               document.getElementById("img1").src = "/image/Css-1.jpg";
+               document.getElementById("h2").innerHTML = " Defination of CSS ";
+               document.getElementById("demo").innerHTML = "' " + "CSS" + " '" + "<br>" + "<br>" + "• Stands for Cascading Style Sheet are used to format the layout of Web pages. They can be used to define text styles, table sizes, and other aspects of Web pages that previously could only be defined in a page's HTML, This is why most Web pages today incorporate cascading style sheets."; 
+           }
+           function myFunc3() {
+               document.getElementById("img1").src = "/image/Js-1.jpg";
+               document.getElementById("h2").innerHTML = " Defination of JavaScript ";
+               document.getElementById("demo").innerHTML = "' " + "JAVASCRIPT" + " '" + "<br>" + "<br>" + "• JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else." + "<br>" + "(Okay, not everything, but it is amazing what you can achieve with a few lines of JavaScript code.)";
+           }
